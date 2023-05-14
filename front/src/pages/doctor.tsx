4b9812efc58b0b27f94a213cc386457f3695479d
Augusto Messina <84212395+augusMessina@users.noreply.github.com>
2023-05-14 20:23:26 +0200

    add explanation

--- front/src/pages/doctor.tsx
+++ front/src/pages/doctor.tsx
@@ -1,6 +1,10 @@
 // la página es CSR porque tanto la query como las mutations utilizadas
 // necesitan varios parámetros que el cliente debe dar, siendo estos año,
-// mes y día (y hora para las mutations addSlot y removeSlot).
+// mes y día (y hora para las mutations addSlot y removeSlot). Se podría hacer
+// la query getAvailableSlots con SSR, utilizando varias páginas con el estilo /[año]/[mes]/[dia],
+// pero no sería para nada eficiente, teniendo que reescribir el mismo código
+// varias veces y creando numerosas páginas cuando en realidad solo se necesita
+// una. En cualquier caso, las mutations deben ser por CSR obligatoriamente.
 
 import DoctorData from '@/components/doctorData';
 
--- front/src/pages/patient.tsx
+++ front/src/pages/patient.tsx
@@ -1,6 +1,10 @@
 // la página es CSR porque tanto la query como la mutation utilizadas
 // necesitan varios parámetros que el cliente debe dar, siendo estos año,
-// mes y día (y hora y DNI para la mutation bookSlot).
+// mes y día (y hora y DNI para la mutation bookSlot). Se podría hacer
+// la query getAvailableSlots con SSR utilizando varias páginas con el estilo /[año]/[mes]/[dia],
+// pero no sería para nada eficiente, teniendo que reescribir el mismo código
+// varias veces y creando numerosas páginas cuando en realidad solo se necesita
+// una. En cualquier caso, la mutation deben ser por CSR obligatoriamente.
 
 import PatientData from '@/components/patientData';
 
